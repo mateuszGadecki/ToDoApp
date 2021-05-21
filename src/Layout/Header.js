3b57250ca1cbfe6d8400979531d2733825da1e2c
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-05-21 09:59:13 +0200

    Adding the ability to switch between light and dark theme

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,12 +1,26 @@
+import { useState, useEffect } from "react";
+
 import classes from "./Header.module.css";
 import sunIcon from "../assets/source_icons_sun-light.svg";
+import moonIcon from "../assets/source_icons_half-moon.svg";
 
 const Header = (props) => {
+  const [isLight, setIsLight] = useState(false);
+  const [icon, setIcon] = useState(sunIcon);
+  useEffect(() => {
+    document.body.classList.toggle("light", isLight);
+  }, [isLight, icon]);
+
+  const themeHandler = () => {
+    setIsLight(!isLight);
+    !isLight ? setIcon(moonIcon) : setIcon(sunIcon);
+  };
+
   return (
     <header className={classes.header}>
       <h1 className={classes.header__title}>TODO</h1>
-      <button className={classes.header__btn}>
-        <img src={sunIcon} alt="sunIcon" />
+      <button onClick={themeHandler} className={classes.header__btn}>
+        <img src={icon} alt="sunIcon" />
       </button>
     </header>
   );
