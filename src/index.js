a522f94503554eeefdfaf5cc0216c88702af0f79
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-05-22 10:04:02 +0200

    Adding store and middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+import App from "./App";
+import toDoListReducer from "./store/reducers/toDoList";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  toDoListReducer,
+  composeEnhancers(applyMiddleware(thunk))
 );
+
+const app = (
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 
 reportWebVitals();
